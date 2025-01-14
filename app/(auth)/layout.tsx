5ce131912df96ef6bdf14a40a1c62bf3c3e7b7e3
LokeshMehar <meharlokesh395@gmail.com>
2025-01-14 20:25:13 +0530

    basic setups

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,10 @@
-// import Logo from "@/components/Logo";
+import Logo from "@/components/Logo";
 import React, { ReactNode } from "react";
 
 function layout({ children }: { children: ReactNode }) {
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center">
-      {/* <Logo /> */}
+      <Logo />
       <div className="mt-12">{children}</div>
     </div>
   );
