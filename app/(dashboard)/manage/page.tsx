bd7f30742070a3fa3c50e14b07869e99258f5146
LokeshMehar <meharlokesh395@gmail.com>
2025-02-12 23:02:59 +0530

    nikal diya tested css now  betteron dployment

--- app/(dashboard)/manage/page.tsx
+++ app/(dashboard)/manage/page.tsx
@@ -25,7 +25,7 @@ function page() {
     <>
       {/* HEADER */}
       <div className="border-b bg-card">
-        <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+        <div className="py-8">
           <div>
             <p className="text-3xl font-bold">Manage</p>
             <p className="text-muted-foreground">
